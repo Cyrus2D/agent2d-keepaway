98c6efb3b009ca8600e1d021487030c828ee395f
arefsa <arefsayareh@gmail.com>
2020-03-04 03:28:57 +0330

    init

---
+++ src/keepaway-keeper-debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./keepaway-keeper.sh --offline-logging --debug --debug-server-connect ${1+"$@"}
---
+++ src/keepaway-taker-debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./keepaway-taker.sh --offline-logging --debug --debug-server-connect ${1+"$@"}
